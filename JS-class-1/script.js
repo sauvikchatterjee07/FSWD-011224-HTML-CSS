bbe0ab25dd4dcb1cb9c98a41a4789c0b6177d7f7
sauvikchatterjee07 <sauvikchatterjee9@gmail.com>
2024-12-21 11:14:55 +0530

    JS class 1

---
+++ JS-class-1/script.js
@@ -0,0 +1,87 @@
+// console.log("Hiiii Hello")
+
+// var
+// let & const
+
+// Primitive:
+
+// 1. Number
+// 2. string
+// 3. Bool
+// 4. BigInt
+// 5. Null
+// 6. Undefined
+// 7. Symbol
+
+
+// NaN
+
+// Non Primitive Type Data:
+
+// 1. Array
+// 2. Object
+
+// var a = 10
+
+// console.log(a, typeof(a), typeof(typeof(a)));
+
+
+//typeof(typeof(anything)) is always String
+
+// var b = String(a)
+// console.log(b, typeof(b));
+
+// ""
+// ''
+// ~ -> tilde
+
+// `` -> back tick
+
+// console.log("Bengal Coding Academy")
+// console.log('Bengal Coding Academy')
+
+// var name = "Pol"
+
+// console.log(`Welcome to Bengal Coding Academy ${name}, How are your ${name}`)
+
+// console.log("Welcome to Bengal Coding Academy " + name, ", How are you " +name);
+
+// isHungry = false
+
+// console.log(typeof(isHungry));
+
+// console.log(typeof(num));
+
+// console.log(typeof(null));
+
+// console.log(0/0);
+
+// console.log(typeof(NaN));
+
+// console.log(isNaN("10000"))
+
+
+// String:
+
+// 1. trim()
+// 2. charAt()
+// 3. cancat()
+// 4. indexOf()
+// 5. lastIndexOf()
+
+// Slice, Splice
+
+
+
+
+// Hoisting
+// Closure
+// Async JS
+// higher order function
+// Map filter reduce
+
+
+
+
+
+
