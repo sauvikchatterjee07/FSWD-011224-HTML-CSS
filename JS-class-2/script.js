8e6067d2865e73590ef2aa7cd44226455432cabf
sauvikchatterjee07 <sauvikchatterjee9@gmail.com>
2024-12-28 08:52:12 +0530

    latest

---
+++ JS-class-2/script.js
@@ -0,0 +1,154 @@
+// console.log("Hiiii Hello")
+
+// var
+// let & const
+
+// Primitive:
+
+// 1. Number
+// 2. string
+// 3. Bool
+// 4. BigInt
+// 5. Null
+// 6. Undefined
+// 7. Symbol
+
+
+// NaN
+
+// Non Primitive Type Data:
+
+// 1. Array []
+
+// 2. Linear DS
+// 1. Homogeneous / Heterogeneous 
+// 3. Indexed Collection Elements
+// 4. Contiguous Memory Aloocation
+
+// let arr = [10, 20, [1, 2, 3], 40, "Hello World", true, false]
+
+// let arr1 = [[1, 2, 3],
+//             [4, 5, 6],
+//             [7, 8, 9]];
+
+// console.log(arr1);
+
+// arr2 = [1, 2, 3]
+
+
+// console.log(arr[4]);
+// console.log(arr[2][1]);
+
+
+// var a = 10
+
+// 2. Object
+
+//If this is a JavaScript OBJ then your Key may not be String
+// JSON -> JS Object Notation
+
+Key:Value
+
+let obj = {
+    "name": "Bengal Coding Academy",
+    "Age": 2,
+    "Courses": ["FSWD", "C/C++ with DSA", "Python Data Science"],
+    "location": "Kolkata",
+    "isVirtual": true,  
+};
+
+console.log(obj.location);
+console.log(obj.location);
+console.log(obj.location);
+
+
+
+
+
+// var a = 10
+
+// console.log(a, typeof(a), typeof(typeof(a)));
+
+
+//typeof(typeof(anything)) is always String
+
+// var b = String(a)
+// console.log(b, typeof(b));
+
+// ""
+// ''
+// ~ -> tilde
+
+// `` -> back tick
+
+// console.log("Bengal Coding Academy")
+// console.log('Bengal Coding Academy')
+
+// var name = "Pol"
+
+// console.log(`Welcome to Bengal Coding Academy ${name}, How are your ${name}`)
+
+// console.log("Welcome to Bengal Coding Academy " + name, ", How are you " +name);
+
+// isHungry = false
+
+// console.log(typeof(isHungry));
+
+// console.log(typeof(num));
+
+// console.log(typeof(null));
+
+// console.log(0/0);
+
+// console.log(typeof(NaN));
+
+// console.log(isNaN("10000"))
+
+
+// String:
+
+
+
+
+
+// 1. trim()
+let str ="Bengal Coding Academy";
+
+// console.log(str.trim());
+// 2. charAt()
+// console.log(str.charAt(str.length + 2));
+
+// 3. concat()
+
+let a = "hello"
+let b = "world"
+
+let c = b.concat(a);
+
+// console.log(c);
+
+// 4. indexOf()
+
+// console.log(a.indexOf('l'));
+
+
+// 5. lastIndexOf()
+
+// console.log(a.lastIndexOf('l'));
+
+// Slice, Splice
+
+
+
+
+// Hoisting
+// Closure
+// Async JS
+// higher order function
+// Map filter reduce
+
+
+
+
+
+
